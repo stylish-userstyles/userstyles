d9e63494a8f1207d594ce0f5f81778114f6ed757
Jason Barnabe <jason.barnabe@gmail.com>
2013-11-24 15:20:20 -0600

    Fix refresh meta script

---
+++ runnerscripts/refresh_meta.rb
@@ -0,0 +1,13 @@
+Style.active.includes(:style_code, {:style_options => :style_option_values}).find_in_batches(batch_size: 100) do |styles_batch|
+	styles_batch.each do |style|
+		style.refresh_meta
+		if !style.changed?
+			puts "Unchanged " + style.id.to_s
+		elsif !style.save(:validate => false)
+			puts "Couldn't save " + style.id.to_s
+		else
+			puts "Saved " + style.id.to_s
+		end
+	end
+	puts "Completed up to #{styles_batch.last.id}"
+end
