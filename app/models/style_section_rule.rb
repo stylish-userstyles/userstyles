2bfe21401831d1dd210cd9c5ebd406cb6759b29d
Jason Barnabe <jason.barnabe@gmail.com>
2013-12-08 18:26:50 -0600

    Truncate style section rule attributes to column length

--- app/models/style_section_rule.rb
+++ app/models/style_section_rule.rb
@@ -1,5 +1,14 @@
 class StyleSectionRule < ActiveRecord::Base
 
+	before_save :truncate_values
+	def truncate_values
+		['rule_type', 'rule_value'].each do |column|
+			next if self[column].nil?
+			length = StyleSectionRule.columns_hash[column].limit
+			self[column] = self[column][0..length-1] if self[column].length > length
+		end
+	end
+
 	def to_userjs_includes
 		if rule_type == 'domain'
 			return ["http://" + self.rule_value + "/*", "https://" + self.rule_value + "/*", "http://*." + self.rule_value + "/*", "https://*." + self.rule_value + "/*"]
