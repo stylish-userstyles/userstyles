ce120d4db3b602a3da85d9c24efafb33ff19749a
Jason Barnabe <jason.barnabe@gmail.com>
2014-01-09 10:27:28 -0600

    issue 3 Include selectors for descendants of form elements

--- runnerscripts/check_agentsheet.rb
+++ runnerscripts/check_agentsheet.rb
@@ -3,11 +3,25 @@ require 'csspool'
 def doc_has_agent_specific_stuff(doc)
 	doc.rule_sets.each do |rs|
 		rs.selectors.each do |s|
+			parent_has_anon = false
 			s.simple_selectors.each do |ss|
 				return true if ss.is_a?(CSSPool::Selectors::Type) and ss.name == 'scrollbar'
 				ss.additional_selectors.each do |as|
 					return true if as.is_a?(CSSPool::Selectors::Type) and as.name == 'scrollbar'
 				end
+				if parent_has_anon
+					# these combinators indicate this is not a child of the previous element
+					if [:+, :~].include?(ss.combinator)
+						parent_has_anon = false
+					# option is not anonymous
+					elsif ss.is_a?(CSSPool::Selectors::Type) and ss.name == 'option'
+						parent_has_anon = true
+						next
+					else
+						return true
+					end
+				end
+				parent_has_anon = true if ss.is_a?(CSSPool::Selectors::Type) and ['input', 'select', 'option', 'textarea'].include?(ss.name)
 			end
 		end
 	end
