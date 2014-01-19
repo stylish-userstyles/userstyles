a964e73537583b0d2e3bac95abac0c7208c5031c
Jason Barnabe <jason.barnabe@gmail.com>
2014-01-18 23:15:21 -0600

    issue 33 Don't allow whitespace-only names

--- runnerscripts/clear_whitespace.rb
+++ runnerscripts/clear_whitespace.rb
@@ -1,9 +1,12 @@
 Style.active.where("short_description not regexp '.*[[:graph:]]+$' or short_description not regexp '^[[:graph:]]+.*'").each do |style|
-	puts "#{style.id} #{style.short_description}"
-	style.save(:validate => false)
+	style.short_description = style.short_description + ' '
+	style.save(:perform_validation => false)
+	puts "#{style.id} #{style.errors.full_messages} #{style.short_description}"
+	
 end
 
 User.where("name not regexp '.*[[:graph:]]+$' or name not regexp '^[[:graph:]]+.*'").each do |user|
-	puts "#{user.id} #{user.name}"
-	user.save(:validate => false)
+	user.name = user.name +  ' '
+	user.save(:perform_validation => false)
+	puts "#{user.id} #{user.errors.full_messages} #{user.name}"
 end
