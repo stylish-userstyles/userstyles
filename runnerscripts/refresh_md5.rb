b71b454e07734fb7080a19e467976533a27b48bc
Jason Barnabe <jason.barnabe@gmail.com>
2014-02-25 21:57:43 -0600

    Always write md5 when refreshing

--- runnerscripts/refresh_md5.rb
+++ runnerscripts/refresh_md5.rb
@@ -2,6 +2,7 @@ Style.includes(:style_code, :style_settings => [:style_setting_options]).find_in
 	styles_batch.each do |style|
 		style.md5 = style.calculate_md5
 		style.save(:validate => false)
+		style.write_md5
 	end
 	puts "Completed up to #{styles_batch.last.id}"
 end
