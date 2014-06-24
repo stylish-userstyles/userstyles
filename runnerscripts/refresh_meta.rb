f35fd848fe38d28c832ffe06d4bc1b18819d8c66
Jason Barnabe <jason.barnabe@gmail.com>
2014-06-24 10:11:55 -0500

    Fix refresh meta script

--- runnerscripts/refresh_meta.rb
+++ runnerscripts/refresh_meta.rb
@@ -1,4 +1,4 @@
-Style.active.includes(:style_code, {:style_options => :style_option_values}).find_in_batches(batch_size: 100) do |styles_batch|
+Style.active.includes(:style_code, {:style_settings => :style_setting_options}).find_in_batches(batch_size: 100) do |styles_batch|
 	styles_batch.each do |style|
 		puts "Checking #{style.id}"
 		style.refresh_meta
