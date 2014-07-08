e5d5fe50d8609b086a532178aa8aa86d813d2ddd
Jason Barnabe <jason.barnabe@gmail.com>
2014-07-07 22:13:24 -0500

    Fix forum login with Chrome

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -291,7 +291,7 @@ class LoginController < ApplicationController
 		secure = true # this should be true unless you are testing.
 		json = JsConnect.getJsConnectString(user, self.params, client_id, secret, secure)
 
-		render :text => json
+		render :json => json
 	end
 
 private
