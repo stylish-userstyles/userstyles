652b6f496aa64fa9fba97bf3dc2cdd13042c48d9
Jason <jason.barnabe@gmail.com>
2015-05-22 22:06:05 -0500

    Fix Rails 5 deprecation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Userstyles::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   config.assets.compress = true
   config.assets.js_compressor  = :uglifier
