0ba967c8af115cb147f808ec4ac495e3c542bc1b
justjup <justin.jupiter.hindman@gmail.com>
2016-11-24 17:19:25 +0200

    misc controller

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -7,4 +7,17 @@ class MiscController < ApplicationController
 	def terms
 		@page_title = "Stylish - Terms of Use"
 	end
+
+private
+	def public_action?
+		true
+	end
+
+	def admin_action?
+		false
+	end
+
+	def verify_private_action(user_id)
+		false
+	end
 end
